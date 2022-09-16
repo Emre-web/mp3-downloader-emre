7fe691fd5f31f773a37e6fc4c56df88b16702d4c
Emre Yıldırım <Emreyildirimbro@gmail.com>
2022-09-16 22:24:41 +0300

    React projesi typeScript ile oluşturuldu. Dosya yapıları ve paket kurulumları tamamlandı.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./Home";
+import Login from "./pages/Login";
+import Register from "./Register";
 
 
 function App() {
   return (
-    <div className="App">
-
-    <h2>hello</h2>
-
-    </div>
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="src/pages/Login" element={<Login />} />
+            <Route path="Register" element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
