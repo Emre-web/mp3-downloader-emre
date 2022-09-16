6b556b0dba9e507ddd5ecb21d06a5e6c98989bec
Emre Yıldırım <Emreyildirimbro@gmail.com>
2022-09-17 01:26:55 +0300

    Input düzeltildi.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,17 @@
 import React, {useState} from 'react';
-import Form from "../Form/Form";
-
 
 function Input() {
-
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     return (
-            <>
-            <input className="input" type="text" placeholder="E-posta adresinizi giriniz." value={email} onChange={e => setEmail(e.target.value)}/>
-            <input className="input" type="password" placeholder="Parolanızı giriniz." value={password} onChange={e => setPassword(e.target.value)}/>
+        <>
+            <input className="input" type="text" placeholder="E-posta adresinizi giriniz." value={email}
+                   onChange={e => setEmail(e.target.value)}/>
+            <input className="input" type="password" placeholder="Parolanızı giriniz." value={password}
+                   onChange={e => setPassword(e.target.value)}/>
 
-            </>
+        </>
     );
 }
 
